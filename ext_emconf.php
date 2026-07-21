1572802e799aee6391aa2f93be5f74f3753e5d8e
jwtue <mail@jonaswolf.de>
2026-07-21 22:37:55 +0200

    TYPO3 v13-Kompatibilität: Constraints erweitern, Version 13.0.0
    - composer.json: typo3/cms-core und -felogin auf "^12.4 || ^13.4"
    - ext_emconf.php: typo3/felogin auf 12.4.0-13.4.99, vhs 7.0.0-7.99.99,
      Version auf 13.0.0 (Schema: Major folgt der höchsten unterstützten TYPO3-Version)

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => '',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '12.0.0',
+    'version' => '13.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
-            'felogin' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'felogin' => '12.4.0-13.4.99',
             'vhs' => '7.0.0-7.99.99',
         ],
         'conflicts' => [],
