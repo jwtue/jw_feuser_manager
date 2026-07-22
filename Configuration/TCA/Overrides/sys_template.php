9d5a4ad9ee1de1eec41ba512a09d61636a382876
jwtue <mail@jonaswolf.de>
2026-07-22 10:11:58 +0200

    v14: obsolete Static-Template-Registrierung entfernt (Site-Set ersetzt sys_template.php)

--- Configuration/TCA/Overrides/sys_template.php
+++
@@ -1,9 +0,0 @@
-<?php
- defined('TYPO3') or die;
-
-// Add an entry in the static template list found in sys_templates for static TS
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-   'jw_feuser_manager',
-   'Configuration/TypoScript',
-   'JW Frontend User Manager'
-);
